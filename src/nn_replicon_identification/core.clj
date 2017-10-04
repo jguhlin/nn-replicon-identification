3767c7d196df40782b0efad34a790baeffbda201
jguhlin <joseph.guhlin@gmail.com>
2017-10-04 09:32:50 -0500

    Network working, no convolution yet though

--- src/nn_replicon_identification/core.clj
+++ src/nn_replicon_identification/core.clj
@@ -13,11 +13,14 @@
             [cortex.optimize.adam :as adam]
             [cortex.experiment.train :as experiment-train]
             [cortex.nn.execute :as execute]
-            [clojure.math.numeric-tower :as math]))
+            [clojure.math.numeric-tower :as math]
+            [biotools.fasta :as fasta]))
+            
+            
 
-; (mat/set-current-implementation :vectorz)
+(mat/set-current-implementation :vectorz)
 
-(def k 7)
+(def k 5)
 (def space (math/expt 5 k))
 
 ; Use base 5, 0 is \N, etc...
@@ -64,19 +67,35 @@
                     (range)
                     (map 
                       convert-sequence
-                      (partition 100 seq-to-analyze)))]
+                      (partition 1000 seq-to-analyze)))]
     small-seq))
 
-(defn training-set [seqs-to-analyze label]
+(defn training-set [seq-to-analyze label]
   (let [seqs (partition 
-               100 
-               (Math/floor (* k 1.8)) 
-               seqs-to-analyze) ; More aggressive partition here for better training....
-        training-data (r/foldcat
-                        (r/map
-                          (fn [x] {:data (convert-sequence x) :label label})
-                          seqs))]
-    training-data))
+               1000
+               1000 ;(Math/floor (* k 1.8)) ; Use more aggressive partitioning for better training?
+               seq-to-analyze)]
+    (r/foldcat
+      (r/map
+        (fn [x] {:data (convert-sequence x) :label label})
+        seqs))))
+
+(def categories 
+  {:Main   [1.0 0.0 0.0 0.0]
+   :pSymA  [0.0 1.0 0.0 0.0]
+   :pSymB  [0.0 0.0 1.0 0.0]
+   :others [0.0 0.0 0.0 1.0]})
+
+(defn create-training-set-from-fasta-file [file]
+  (with-open [rdr (clojure.java.io/reader file)]
+    (apply concat
+      (doall
+        (map 
+          (fn [x]
+            (training-set 
+              (clojure.string/upper-case (:seq x))
+              (get categories (keyword (:id x)))))
+          (fasta/parse rdr))))))
 
 (def params
   {:test-ds-size      5000
@@ -85,77 +104,92 @@
    :epoch-count       50
    :epoch-size        200000})
 
-(def network-description
-  [(layers/input 1000 1 1 :id :data)
-   (layers/convolutional space 0 1 20)
-   (layers/max-pooling 2 0 2)
-   (layers/dropout 0.9)
-   (layers/relu)
-   (layers/convolutional 5 0 1 50)
-   (layers/max-pooling 2 0 2)
-   (layers/batch-normalization)
-   (layers/linear 1000)
-   (layers/relu :center-loss {:label-indexes {:stream :label}
-                              :label-inverse-counts {:stream :label}
-                              :labels {:stream :labels}
-                              :alpha 0.9
-                              :lambda 1e-4})
-   (layers/dropout 0.5)
-   (layers/linear 5)
-   (layers/softmax :id :label)])
+;(def network-description
+;  [(layers/input 1000 1 1 :id :data)
+;   (layers/convolutional space 0 1 20)
+;   (layers/max-pooling 2 0 2)
+;   (layers/dropout 0.9)
+;   (layers/relu)
+;   (layers/convolutional 5 0 1 50)
+;   (layers/max-pooling 2 0 2)
+;   (layers/batch-normalization)
+;   (layers/linear 1000)
+;   (layers/relu :center-loss {:label-indexes {:stream :label}
+;                              :label-inverse-counts {:stream :label}
+;   :labels {:stream :labels}
+;   :alpha 0.9
+;   :lambda 1e-4
+;   (layers/dropout 0.5)
+;   (layers/linear 5)
+;   (layers/softmax :id :label)})
 
 (def nn
   (->
-    [(layers/input 5 1 1 :id :data)
-     (layers/convolutional space 0 1 100)
-;     (layers/max-pooling 2 0 2)
-;     (layers/dropout 0.9)
-;     (layers/relu)
-;     (layers/convolutional 5 0 1 50)
-;     (layers/max-pooling 2 0 2)
-;     (layers/batch-normalization)
-;     (layers/linear 1000)
-;     (layers/dropout 0.5)
-     (layers/linear 3)
+    [(layers/input space 1 1 :id :data)
+     ; (layers/convolutional 5 0 1 5)
+     ;     (layers/max-pooling 2 0 2)
+     ;     (layers/dropout 0.9)
+     ;     (layers/relu)
+     ;     (layers/convolutional 5 0 1 20)
+     ;     (layers/max-pooling 2 0 2)
+     ;     (layers/batch-normalization)
+     ;     (layers/linear 50)
+     ;     (layers/dropout 0.5)
+     (layers/linear 100)
+     (layers/linear 50)
+     (layers/linear 4)
      (layers/softmax :id :label)]
    network/linear-network))
 
-; This is the more complicated "Example"
-(def train-orig
-  (concat
-    (training-set seq-main [1.0 0.0 0.0])
-    (training-set seq-psyma [0.0 1.0 0.0])
-    (training-set seq-psymb [0.0 0.0 1.0])))
+;(def train-orig2
+;  (concat
+;  (training-set seq-main [1.0 0.0 0.0])
+;  (training-set seq-psyma [0.0 1.0 0.0])
+;  (training-set seq-psymb [0.0 0.0 1.0]))
 
-(def train-test
-  (concat
-    (for [i (range 1000)]
-      {:data (mat/matrix (take 5
-                           (repeatedly #(rand-int 2))))
-       :label :main})
-    (for [i (range 1000)]
-      {:data (mat/matrix (take 5
-                           (repeatedly #(rand-int 2)))) 
-       :label :psyma})
-    (for [i (range 1000)]
-      {:data (mat/matrix (take 5
-                           (repeatedly #(rand-int 2)))) 
-       :label :psymb})))
+;(def train-test)
+;  (concat))
+;    (for [i (range 100)])))
+;      {:data (take 5 (repeatedly #(rand-int 2)))})))
+;       :label [1.0 0.0 0.0]})))
+;    (for [i (range 100)])))
+;      {:data (take 5 (repeatedly #(rand-int 2)))}))) 
+;       :label [0.0 1.0 0.0]})))
+;    (for [i (range 100)])))
+;      {:data (take 5 (repeatedly #(rand-int 2)))}))) 
+;       :label [0.0 0.0 1.0]})))
+
+;(println train-test)
+
+(def kh35c-main "GGGAGGTCGGTGCGCTTGGGGCCTACGGCTATCACGACGCCGTCGATTTCACGCCGACGCGCGTGCCGGAAGGCCAGAAATGCGCCGTCGTGCGCAACTATTATGCCCATCATCACGGCATGTCGGTCGCCGCGGTCGCCAATGTCGTCTTCAACGGGCAGCTGCGCGAGTGGTTCCACGCCGATCCCGTCATCGAGGCCGCCGAACTCCTCCTGCAGGAAAAGGCCCCGCGTGACATCCCGGTCATGGCAGCCAAGCGCGAGCCGGAAGCGCTGGGCAAGGGCCAGGCCGATCTCCTGCGCCCCGAAGTCCGCGTCGTCGAAGACCCGATCAATCAGGACCGCGAGACGGTGCTTCTGTCGAACGGTCACTACTCCGTCATGTTGACGGCGACAGGGGCGGGCTATGCCCGCTGGAACGGCCAGTCGGTCACGAGATGGACTCCGGACCCGGTAGAGGACAGGACGGGGACCTTCATCTTCCTTCGCGACACGGTGACGGGCGACTGGTGGTCGGCCACGGCCGAGCCCCGGCGTGCGCCGGGCGAAAAGACCGTTACCCGCTTCGGCGACGACAAGGCCGAATTCGTCAAGACCGTCGGCGATCTGACAAGCGAGGTGGAATGCATCGTCGCGACCGAGCACGATGCCGAAGGCCGCCGGGTTATCCTGCTCAACACGGGCACGGAAGACCGGTTCATCGAGGTGACCTCCTATGCCGAGCCGGTGCTTGCGATGGACGATGCCGACAGCTCGCACCCGACCTTCTCGAAGATGTTCCTGCGCACCGAGATCAGCCGTCACGGAGACGTGATATGGGTCTCGCGCAACAAGCGAAGCCCCGGCGATCCGGACATCGAGGTCGCCCATCTCGTCACCGACAATGCCGGCAGCGAGCGCCACACGCAGGCGGAAACCGATCGCCGGCGCTTCCTCGGCCAGGGCCGCACGCTTGCCGAGGCGGCCGCATTCGACCCGGGCGCCACGCTTTCCGGCACCGACGGCTTCACGCTCGATCCGATCGTGTCGCTCCGCCGCGTCGTACGCGTGCCGGCGGGCAAGAAAGTGAGCGTCATCTTCTGGACGATCGCCGCCCCGGACAGGGAAGGCGTCGACCGGGCGATCGACCGCTACCGGCATCCGGAAACCTTCAATCACGAGCTCATCCATGCCTGGACCCGCAGCCAGGTGCAGATGCGCCATGTCGGGATCACCTCGAAGGAGGCCGCGAGCTTCCAGATGCTCGGCCGCTATCTCGTCTATCCGGATATGCACCTTCGCGCCGACGCGGAGACCGTCAAGACCGGGCTCGCCTCGCAATCGGCGCTGTGGCCGCTGGCGATCTCCGGCGACTTCCCGATCTTCTGCCTCAGGATCAACGACGACGGCGATCTCGGCATCGCCCGCGAGGCCTTGCGGGCGCAGGAATATCTGAGAGCTCGCGGCATCACCGCCGATCTGGTGGTCGTCAACGAGCGCGCCTCCTCCTACGCGCAGGACCTGCAGCACACGCTCGACTCGATGTGCGAGAATTTGAGGCTTCGCGGCCTTTCGGACGGCCCGCGCCAGCACATATTTGCGGTGCGCCGGGACCTTATGGAACCGGAAACCTGGTCGACGCTGATCTCGGCATCCCGCGCCGTCTTCCATGCGCGCAACGGCACGATCTCGGATCAGATAGCCCGCGCCACATCGCTCTACTCCAAACCTTCCGAAAAGAAGGAGGAGGGCGCCGAGATGCTGCTGCCGGTGATACGGGAG")
+(def wsm419-uni2 "CGTCAACGAGCTGCTCACCAACGCGCTCAAGCATGCTTTCAACGGCCGCGAAGGAGGAGTAATCACGCTGCGAAGCACTTTTGAGGATGATGGCTACCGTGTCATCGTTGCGGACGACGGAATAGGTTTCCCGGACGGAGAGACCTGGCCCAAACACGGCAAGCTTGGCGAGTTGATCGCGCAGTCGCTTCGCGAAAATTCCAGGGCTGATCTCCAGGTGATCTCCACGCCGGGTCAAGGCACACGCGCAACGATTCGTTTCCGGAACGACTCCGTATAGGCGCGCGAGCGTCCGAGCACCGCGCATCATAAGGCCGCGCGCGAACGCGGCTGCATAGGATGTCTATCGGCCCGGCTTATAGATCTGGTCGAAAATCCCCCATCGTCGAAGAACTTCGGCTGGGCTTCCTGCCAACCGCCGAAGTCGCCAATGGTGACCAGTTTGAGATCGGCAAAGCGTGCCGAGTCCGCGGGGTCGGCCAGCTCGGGCTTGAACGGCCGATAGTAGTGCTTGGCGACGATCTTCTGGCCGACGTCGCTGTAGAGGTAGCCGAGATAGGCTTCGGCAACATTGCGGGTGCCTTTGCTGTCGACATTCCCGTCCAAGAGCGCCACGGAGGGCTCGGCCCTGATCGATATGGACGGTGTCACGATCTCGAACTTGTCGGGGCCGAGTTCATCGAGCGCGAGATAGGCCTCATTCTCCCAGGCGAGCAGCACGTCGCCGAGCCCGCGATGGACGAAAGTGGTCATCGCTCCCCACGCGCCGGTGTCGAGAACGAGAACCTGCTTGAAAAGCGCCGCCGCATATTCCTGCGCCTTGGCCTCGTCGCCGTTGTTTGCATCCCGCGCCCAGGCCCAGGCTGCAAGGAAGTTCCAGCGCGCGCCACCCGAGGTCTT")
+
+;println "Generating Training Set")
+
+(def train-orig (create-training-set-from-fasta-file "data-files/Rm1021.final.fasta"))
 
 
-(count train-orig)
-(first train-orig)
+;(println (count train-orig))
 
-(try
-  (def trained
-    (experiment-train/train-n 
-      nn
-      train-test
-      train-test
-      :batch-size 200 :epoch-count 100))
-  (catch Exception e
-   (println e)))
+(println (first train-orig))
+(println (reduce + (map mat/esum (map :data train-orig))))
+;(println (map :label train-orig))
+
+;(println train-orig2)
+
+;(System/exit 0)
 
 (defn -main []
-  (println "done"))
+  (try
+    (def trained
+      (experiment-train/train-n 
+        nn
+        train-orig
+        train-orig
+        :batch-size 5000 :epoch-count 500))
+    (catch Exception e
+      (println e)))
+  
+  (println (execute/run trained [{:data (convert-sequence kh35c-main)}])))
+                                 
 
